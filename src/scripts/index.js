440b48a0edff384604666d5458728c9b3bede6ab
abyanardiatama <abyanardiatama@students.undip.ac.id>
2023-11-20 23:57:01 +0700

    finish proyek 2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,20 @@
-import 'regenerator-runtime'; /* for async await transpile */
+import 'regenerator-runtime';
+import '../styles/style.css';
+import '../styles/responsive.css';
+import App from './views/app';
+import swRegister from './utils/sw-register';
 
-import '../styles/main.css';
-//import bootstrap icons
-import 'bootstrap-icons/font/bootstrap-icons.css';
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
 
-import "@fontsource/montserrat";
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
 
-// import data.json
-import restaurants from '../public/data/DATA.json';
-
-// display restaurants in DOM
-const restaurantsContainer = document.querySelector('#restaurant');
-const restaurantsList = restaurants.restaurants;
-restaurantsList.forEach((restaurant) => {
-  restaurantsContainer.innerHTML += `
-    <div class="restaurant-item">
-      <div class="card">
-        <img src="${restaurant.pictureId}" class="restaurant-img" alt="${restaurant.name}">
-        <div class="card-body">
-          <h5 class="name">${restaurant.name}</h5>
-          <p class="city"><i class="bi bi-map-fill"></i> ${restaurant.city}</p>
-          <p class="rating"><i class="bi bi-star-fill"></i> ${restaurant.rating}</p>
-          <a href="#" class="detail">Detail</a>
-        </div>
-      </div>
-    </div>
-  `;
+window.addEventListener('load', () => {
+  app.renderPage();
+  swRegister();
 });
